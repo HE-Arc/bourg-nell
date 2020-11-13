32a23cee5b9be91dda6096010d9a510691d41b51
ugo.crucy <ugo.crucy@he-arc.ch>
2020-11-13 10:12:30 +0100

    fix the bug at line 36

--- gameserver/GameBoard/GameBoard.ts
+++ gameserver/GameBoard/GameBoard.ts
@@ -1,7 +1,7 @@
 import {Deck} from "./Cards/Deck";
 import {Player} from "./Player/Player";
 
-const playerNumber = 4;
+const PLAYERNUMBER = 4;
 
 export class GameBoard {
     private deck = new Deck();
@@ -10,8 +10,8 @@ export class GameBoard {
 
     public constructor() {
         this.setScore(0,0);
-        //this.deck.shuffleDeck();
-        for (let i =0; i<playerNumber ;++i) {
+        this.deck.shuffleDeck();
+        for (let i =0; i<PLAYERNUMBER ;++i) {
             this.players.push(new Player("Player" + i));
         }
         this.giveCards();
@@ -31,8 +31,9 @@ export class GameBoard {
     }
 
     public giveCards() {
+        let cardNumber = this.deck.getDeck().length / PLAYERNUMBER;
         this.players.forEach(player => {
-            player.setCards(this.deck.getDeck().splice(0,this.deck.getDeck().length / playerNumber));
+            player.setCards(this.deck.getDeck().splice(0,cardNumber));
         });
     }
 
