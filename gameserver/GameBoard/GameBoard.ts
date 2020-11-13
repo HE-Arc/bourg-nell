ff1716082799cc6fa8460c9a1e8f85f918bf2be4
ugo.crucy <ugo.crucy@he-arc.ch>
2020-11-13 08:59:19 +0100

    added the Player class

--- gameserver/GameBoard/GameBoard.ts
+++ gameserver/GameBoard/GameBoard.ts
@@ -1,12 +1,18 @@
 import {Deck} from "./Cards/Deck";
+import {Player} from "./Player/Player";
 
-class GameBoard {
+export class GameBoard {
     private deck = new Deck();
-    private score = {"team1": 0, "team2":0};
-    // private players = new Array<Player>(4); // TODO Implement Player Class
+    private score = {"team1": 0, "team2": 0};
+    private players = new Array<Player>();
 
     public constructor() {
-        this.init();
+        this.setScore(0,0);
+        //this.deck.shuffleDeck();
+        for (let i =0;i<4;++i) {
+            this.players.push(new Player("Player" + i));
+        }
+        this.giveCards();
     }
 
     public getDeck(){
@@ -22,8 +28,13 @@ class GameBoard {
         this.score["team2"] = team2Score;
     }
 
-    public init() {
-        this.setScore(0,0);
-        this.deck.shuffleDeck();
+    public giveCards() {
+        this.players.forEach(player => {
+            player.setCards(this.deck.getDeck().splice(0,9));
+        });
     }
+
+    public getPlayers() {
+        return this.players;
+    } 
 }
---
+++ gameserver/GameBoard/Player/Player.ts
@@ -0,0 +1,47 @@
+import {CARDS} from "../Cards/Cards";
+import {CARD_COLOR} from "../Cards/CardColor";
+import {CARD_VALUE} from "../Cards/CardValue";
+import {Deck} from "../Cards/Deck";
+
+export class Player {
+    private name: string;
+    private cards = new Array<CARDS>();
+    
+    public constructor(name: string) {
+        this.name = name;
+    }
+
+    public setCards(cards: CARDS[]) {
+        this.cards = cards;
+    }
+
+    public getName() {
+        return this.name;
+    }
+
+    public toString() {
+        let res = "";
+        res = this.name + " [";
+        this.cards.forEach(card => {
+            res += CARD_VALUE[Deck.findCardValue(card)] + "_" + CARD_COLOR[Deck.findCardColor(card)] + " ";
+        });
+        res += "]";
+        return res;
+    }
+
+    public getCards() {
+        return this.cards;
+    }
+
+    public play(card: CARDS) {
+        let index = this.cards.indexOf(card);
+        let result = null;
+        if(index > -1) {
+            result = this.cards[index];
+            this.cards.splice(index, 1);
+        } else {
+            throw new Error(card + " doesn't exist in this player deck");
+        }
+        return result;
+    }
+}
