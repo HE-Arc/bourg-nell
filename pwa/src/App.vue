04a03a4e63a559678ff807647fa20e6b38122992
luca@laissue.ch <luca@laissue.ch>
2020-11-27 11:41:45 +0100

    HistoryItem component

--- pwa/src/App.vue
+++ pwa/src/App.vue
@@ -1,20 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Avatar md5hash="78d172c9cbb993794d2e9021fce57d68" status="online"/>
-  </div>
+    <div id="root" class="account screen">
+        <div class="account-header">
+            <Avatar md5hash="78d172c9cbb993794d2e9021fce57d68" status="online"/>
+            <h2>Luca Laissue</h2>
+        </div>
+        <h3>Games</h3>
+        <HistoryItem v-for="item in games" :key="item.gameId" :gameObj="item"/>
+    </div>
 </template>
 
 <script>
 import Avatar from './components/Avatar';
+import HistoryItem from './components/HistoryItem';
 
 export default {
-  name: 'App',
-  components: {
-    Avatar
-  }
+    name: 'App',
+    components: {
+        Avatar,
+        HistoryItem
+    },
+    data() {
+        return {
+            games: [
+                {
+                    gameId: "123712947123908",
+                    player1: "12398712893789",
+                    player2: "123897128937",
+                    player3: "123897128937",
+                    player4: "123897128937",
+                    scoreLimit: 1000,
+                    scoreTeam1: 560,
+                    scoreTeam2: 1012,
+                    gameState: 2,
+                    creationDate: new Date(Date.now())
+                }
+            ]
+        }
+    }
 }
 </script>
-
-<style>
-</style>
---
+++ pwa/src/components/HistoryItem.vue
@@ -0,0 +1,73 @@
+<template>
+    <div :class="historyItemClass" card-elevation="1">
+        <div class="score">
+            <div class="score-header">
+                <div class="gamestatus">{{gameStatusText}}</div>
+                <div class="date">
+                    <span>{{gameObj.creationDate}}</span>
+                </div>
+            </div>
+            <div class="gameresult">
+                <div class="myscore">{{gameObj.scoreTeam1}}</div>
+                <div class="theirscore">{{gameObj.scoreTeam2}}</div>
+            </div>
+        </div>
+        <div class="hidden-content">
+            <h4>Players</h4>
+            <div class="players">
+                <div class="player">
+                    <Avatar md5hash="78d172c9cbb993794d2e9021fce57d68"/>
+                    <span>Username placeholder</span>
+                </div><div class="player">
+                    <Avatar md5hash="78d172c9cbb993794d2e9021fce57d68"/>
+                    <span>Username placeholder</span>
+                </div><div class="player">
+                    <Avatar md5hash="78d172c9cbb993794d2e9021fce57d68"/>
+                    <span>Username placeholder</span>
+                </div><div class="player">
+                    <Avatar md5hash="78d172c9cbb993794d2e9021fce57d68"/>
+                    <span>Username placeholder</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Avatar from "./Avatar";
+
+    export default {
+        components: {
+            Avatar
+        },
+        props: {
+            gameObj: {
+                type: Object,
+                required: true
+            }
+        },
+        computed: { 
+            historyItemClass() {
+                let classStr = "";
+                switch(this.gameObj.gameState)
+                {
+                    case 2: classStr = "playing"; break;
+                    case 3: classStr = "won"; break;
+                    case 4: classStr = "lost"; break;
+                }
+                return `card history_item ${classStr}`;
+            },
+            gameStatusText() {
+                switch(this.gameObj.gameState)
+                {
+                    case 0: return "Game created";
+                    case 1: return "Game aborted";
+                    case 2: return "Game in progress";
+                    case 3: return "Won team1";
+                    case 4: return "Won team2";
+                    default: return "Error";
+                }
+            }
+        }
+    }
+</script>
