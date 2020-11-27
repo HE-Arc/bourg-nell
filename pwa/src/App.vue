294b2a0e761512cbeb950c26c5e5ab79681e3cb9
luca@laissue.ch <luca@laissue.ch>
2020-11-27 10:28:03 +0100

    Avatar component

---
+++ pwa/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Avatar md5hash="78d172c9cbb993794d2e9021fce57d68" status="online"/>
+  </div>
+</template>
+
+<script>
+import Avatar from './components/Avatar';
+
+export default {
+  name: 'App',
+  components: {
+    Avatar
+  }
+}
+</script>
+
+<style>
+</style>
