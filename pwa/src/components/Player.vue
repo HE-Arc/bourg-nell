8b834d3037c257d6b7d4bae1fb5942d0429f43a0
luca@laissue.ch <luca@laissue.ch>
2020-12-14 15:24:31 +0100

    Player component

---
+++ pwa/src/components/Player.vue
@@ -0,0 +1,34 @@
+<template>
+    <div :class="displayClass" :pos="'pos-'+pos" card-elevation="2">
+        <Avatar md5hash="78d172c9cbb993794d2e9021fce57d68" status="playing"/>
+        <h3>Luca Laissue</h3>
+    </div>
+</template>
+
+<script>
+    import Avatar from "./Avatar";
+
+    export default {
+        name: "Player",
+        components: {Avatar},
+        props: {
+            playerId: {
+                type: Number,
+                required: false,
+            },
+            playing: {
+                type: Boolean,
+                required: true
+            },
+            pos: {
+                type: String,
+                required: true
+            }
+        },
+        computed: { 
+            displayClass() {
+                return `card player${this.playing ? " playing" : ""}`
+            }
+        }
+    }
+</script>
