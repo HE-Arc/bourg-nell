2980f7d34736901439d67a7eff7cffec06dd6f4f
luca@laissue.ch <luca@laissue.ch>
2020-12-15 17:14:23 +0100

    Trump modal

---
+++ pwa/src/components/ChooseTrumpModal.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="trump-selection modal">
+        <div class="choice-modal card" card-elevation="4">
+            <h2 v-if="allowPass">Choose trump</h2>
+            <h2 v-else>Passed ! Choose trump</h2>
+            <div class="choices">
+                <img @click="emitHearts" src="/cards_fr/hearts/ace.png" alt="">
+                <img @click="emitSpades" src="/cards_fr/spades/ace.png" alt="">
+                <img @click="emitDiamonds" src="/cards_fr/diamonds/ace.png" alt="">
+                <img @click="emitClubs" src="/cards_fr/clubs/ace.png" alt="">
+            </div>
+            <button v-if="allowPass" @click="emitPass" class="pass-button">Pass</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ChooseTrumpModal",
+        props: {
+            allowPass: {
+                type: Boolean,
+                required: true
+            }
+        },
+        methods: {
+            emitHearts() {this.$emit("hearts");},
+            emitSpades() {this.$emit("spades");},
+            emitDiamonds() {this.$emit("diamonds");},
+            emitClubs() {this.$emit("clubs");},
+            emitPass() {this.$emit("pass");}
+        }
+    }
+</script>
