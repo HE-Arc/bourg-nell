9f3647f494bbe52c1d167e232869961d2d27a3cf
luca@laissue.ch <luca@laissue.ch>
2020-12-15 17:14:45 +0100

    Implement new events

--- pwa/src/screens/GameScreen.vue
+++ pwa/src/screens/GameScreen.vue
@@ -3,7 +3,7 @@
         <div class="player-cards">
             <PlayingCard 
                 v-for="card in ownCards"
-                @played="tryPlayCard($event)"
+                @played="playCard($event)"
                 :key="card"
                 :card="card"
                 :handPosition="getCardHandPosition(card)"
@@ -16,34 +16,38 @@
             <PlayingCard v-if="playerTopPlayedCard !== null" :card="playerTopPlayedCard" playedDirection="top" :fold="fold"/>
             <PlayingCard v-if="playedCard !== null" :card="playedCard" playedDirection="bottom" :fold="fold"/>
         </div>
-        <Player :playing="player1Playing" name="luca" imgPath="78d172c9cbb993794d2e9021fce57d68" pos="right"/>
-        <Player :playing="player2Playing" name="ugo" imgPath="78d172c9cbb993794d2e9021fce57d68" pos="top"/>
-        <Player :playing="player3Playing" name="robin" imgPath="78d172c9cbb993794d2e9021fce57d68" pos="left"/>
-        <div class="score card" card-elvation="1">
+        <Player :playing="player1Playing" :name="playerRightName" imgPath="78d172c9cbb993794d2e9021fce57d68" pos="right"/>
+        <Player :playing="player2Playing" :name="playerTopName" imgPath="78d172c9cbb993794d2e9021fce57d68" pos="top"/>
+        <Player :playing="player3Playing" :name="playerLeftName" imgPath="78d172c9cbb993794d2e9021fce57d68" pos="left"/>
+
+        <div class="score card" card-elevation="1">
             <h3>Score</h3>
             <div>
-                <h6>Luca &amp; Robin</h6>
+                <h6>You &amp; {{playerTopName}}</h6>
                 <div>{{myTeamScore}}</div>
             </div>
             <div>
-                <h6>Luca &amp; Robin</h6>
+                <h6>{{playerRightName}} &amp; {{playerLeftName}}</h6>
                 <div>{{theirTeamScore}}</div>
             </div>
 
         </div>
 
-        <div v-if="showTrumpModal" class="trump-selection modal">
-            <div class="choice-modal card" card-elevation="4">
-                <h2>Choice trump</h2>
-                <div class="choices">
-                    <img src="cards_fr/hearts/ace.png" alt="">
-                    <img src="cards_fr/spades/ace.png" alt="">
-                    <img src="cards_fr/diamonds/ace.png" alt="">
-                    <img src="cards_fr/clubs/ace.png" alt="">
-                </div>
-                <button class="pass-button">Pass</button>
-            </div>
-        </div>
+        <CurrentTrump v-if="currentTrump" :currentTrump="currentTrump"/>
+
+
+
+        <ChooseTrumpModal 
+            v-if="showTrumpModal" 
+
+            @hearts="trumpHearts"
+            @spades="trumpSpades"
+            @diamonds="trumpDiamonds"
+            @clubs="trumpClubs"
+            @pass="pass"
+
+            :allowPass="allowPass"
+        /> 
 
         <div v-if="!connected && allPlayerJoined" class="loading modal">
             <img src="swisscross.png"/>
@@ -56,32 +60,50 @@
 import io from "socket.io-client";
 import PlayingCard from "../components/PlayingCard";
 import Player from "../components/Player";
+import ChooseTrumpModal from "../components/ChooseTrumpModal";
+import CurrentTrump from "../components/CurrentTrump";
+import {CARD_COLOR} from "../cards/CardColor";
 
 export default {
     // Todo : Load games from server
     name: 'GameScreen',
     components: {
         Player,
-        PlayingCard
+        PlayingCard,
+        ChooseTrumpModal,
+        CurrentTrump
     },
     data()
     {
         return {
             myId: 0,
             ownCards: [],
-            playerRightPlayedCard: null,
-            playerLeftPlayedCard: null,
-            playerTopPlayedCard: null,
             playedCard: null,
-            folding: false,
+
+            currentPlayerPlaying: 0,
+            currentTrump: null,
+
+            playerRightPlayedCard: null,
+            playerRightName: null,
+            playerLeftPlayedCard: null,
+            playerLeftName: null,
+            playerTopPlayedCard: null,
+            playerTopName: null,
+
             lang: "fr",
+
+            folding: false,
+            fold: null,
+
             myTeamScore: 0,
             theirTeamScore: 0,
-            fold: null,
-            currentPlayerPlaying: 0,
+
             socket: null,
             connected: false,
+            gameStarted: false,
+
             showTrumpModal: false,
+            allowPass: true
         }
     },
     mounted() {
@@ -95,75 +117,119 @@ export default {
         this.socket.on("disconnect", () => {this.connected = false});
         this.socket.on("reconnect", () => {this.connected = true});
 
-        this.socket.on("cards", this.giveCards);
-        this.socket.on("playCard", this.playCard);
-        this.socket.on("playerPlaying", (id) => {this.currentPlayerPlaying = id});
         this.socket.on("id", this.onId);
-        this.socket.on("fold", this.foldCards)
+        this.socket.on("player", this.onPlayer);
+        this.socket.on("cards", this.onCards);
+
+        this.socket.on("chooseTrump", this.onChooseTrump);
+        this.socket.on("currentTrump", this.onCurrentTrump);
+        this.socket.on("playerPassed", this.onPlayerPassed);
+        this.socket.on("passed", this.onPassed);
+
+        this.socket.on("scoreTeam1", this.onScoreTeam1);
+        this.socket.on("scoreTeam2", this.onScoreTeam2);
+
+        this.socket.on("playCard", this.onPlayCard);
+        this.socket.on("playerPlaying", (id) => {this.currentPlayerPlaying = id});
+        this.socket.on("fold", this.onFoldCards)
     },
     computed: {
         player1Playing() {return this.relativeIndex(this.currentPlayerPlaying) == 1},
         player2Playing() {return this.relativeIndex(this.currentPlayerPlaying) == 2},
         player3Playing() {return this.relativeIndex(this.currentPlayerPlaying) == 3},
-        allPlayerJoined() {return true; return this.player1 && this.player2 && this.player3}
+        allPlayerJoined() {return true; return this.player1 && this.player2 && this.player3},
+        currentTrump() {return CARD_COLOR.SPADES;}
     },
     methods: {
         relativeIndex(index)
         {
             return (4 + index - this.myId ) % 4;
         },
-        foldCards(playerWinning)
-        {
-            setTimeout(() => {
-                this.folding = true;
-                let relativePlayerWinning = this.relativeIndex(playerWinning);
-                if(relativePlayerWinning == 0)
-                {
-                    this.fold = "bottom";
-                }
-                else if(relativePlayerWinning == 1)
-                {
-                    this.fold = "right";
-                }
-                else if(relativePlayerWinning == 2)
-                {
-                    this.fold = "top";
-                }
-                else if(relativePlayerWinning == 3)
-                {
-                    this.fold = "left";
-                }
-                setTimeout(() => {
-                    this.playerRightPlayedCard = null;
-                    this.playerLeftPlayedCard = null;
-                    this.playerTopPlayedCard = null;
-                    this.playedCard = null;
-                    this.fold = null;
-                    this.folding = false;
-                }, 1000)
-            }, 2000);
-        },
-        onId(id)
-        {
-            console.log(id);
-            this.myId = id;
-        },
-        giveCards(cards)
-        {
-            console.log("Cards");
-            console.log(cards);
-            this.ownCards = cards.sort();
-        },
         getCardHandPosition(card)
         {
             let index = this.ownCards.indexOf(card);
             return 9 - this.ownCards.length + (index * 2);
         },
-        tryPlayCard(card)
+
+        // You playe a card
+        playCard(card) { this.socket.emit("turnCard", card); },
+
+        // Trump selection
+        pass() { this.socket.emit("pass"); },
+        trumpHearts() { this.socket.emit("trump", CARD_COLOR.HEARTS); },
+        trumpSpades() { this.socket.emit("trump", CARD_COLOR.SPADES); },
+        trumpDiamonds() { this.socket.emit("trump", CARD_COLOR.DIAMONDS); },
+        trumpClubs() { this.socket.emit("trump", CARD_COLOR.CLUBS); },
+
+        // Your id
+        onId(id) { this.myId = id; },
+
+        // Player joined the game
+        onPlayer(index, name)
         {
-            this.socket.emit("turnCard", card);
+            let relativeIndex = this.relativeIndex(index);
+
+            if(relativeIndex == 1) { this.playerRightName = name; }
+            else if(relativeIndex == 2) { this.playerTopName = name; }
+            else if(relativeIndex == 3) { this.playerLeftName = name; }
         },
-        playCard(playerIndex, card) {
+
+        // You receive your cards
+        onCards(cards) { this.ownCards = cards.sort(); this.currentTrump = null; },
+
+        // You have to choose trump
+        onChooseTrump() { this.allowPass = true; this.showTrumpModal = true; },
+
+        // Trump has been selected
+        onCurrentTrump(trump) {
+            this.currentTrump = trump;
+            this.showTrumpModal = false;
+        },
+
+        // A player passed to his mate
+        onPlayerPassed(index)
+        {
+            let relativeIndex = this.relativeIndex(index);
+            if(relativeIndex == 0)
+            {
+                this.showTrumpModal = false;
+            }
+        },
+
+        // Your mate passed to you
+        onPassed()
+        {
+            this.allowPass = false;
+            this.showTrumpModal = true;
+        },
+
+        // Scores updates
+        onScoreTeam1(score) {if(this.myId % 2 == 0) this.myTeamScore = score; else this.theirTeamScore = score},
+        onScoreTeam2(score) {if(this.myId % 2 != 0) this.myTeamScore = score; else this.theirTeamScore = score},
+
+        // 4 cards on the board, best card fold to the player
+        onFoldCards(playerWinning)
+        {
+            this.folding = true;
+            let relativePlayerWinning = this.relativeIndex(playerWinning);
+            if(relativePlayerWinning == 0) { this.fold = "bottom"; }
+            else if(relativePlayerWinning == 1) { this.fold = "right"; }
+            else if(relativePlayerWinning == 2) { this.fold = "top"; }
+            else if(relativePlayerWinning == 3) { this.fold = "left"; }
+
+            // Wait animation end
+            setTimeout(() => {
+                this.playerRightPlayedCard = null;
+                this.playerLeftPlayedCard = null;
+                this.playerTopPlayedCard = null;
+                this.playedCard = null;
+                this.fold = null;
+                this.folding = false;
+            }, 1000)
+        },
+
+        // A card has been played
+        onPlayCard(playerIndex, card) {
             let relativeIndex = this.relativeIndex(playerIndex);
             if(relativeIndex == 0)
             {
@@ -182,7 +248,7 @@ export default {
             {
                 this.playerLeftPlayedCard = card;
             }
-        }
+        },
     }
 }
 </script>
